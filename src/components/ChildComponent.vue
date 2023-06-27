bd3b6e5fd8c34eff8c2e8a53baa22ae7f8f2c1ce
Sam Hulick <samh@reelcrafter.com>
2023-06-27 11:26:52 -0500

    Initial

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref, onMounted, nextTick } from "vue";
+
+const dataVAttribs = ref<string[]>([]);
+const rootRef = ref<HTMLDivElement | null>(null);
+
+onMounted(async () => {
+  if (!rootRef.value) return;
+
+  await nextTick();
+  dataVAttribs.value = Object.keys(rootRef.value.dataset);
+});
+
+console.log(
+  "Referencing this changes the data-v attribute hash",
+  import.meta.env.SSR
+);
+</script>
+
+<template>
+  <div class="root" ref="rootRef">
+    <div>Howdy, my data-v-* attribs are: {{ dataVAttribs }}</div>
+  </div>
+</template>
+
+<style scoped>
+.root {
+  color: red;
+}
+</style>
---
+++ src/components/ParentComponent.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ChildComponent from "./ChildComponent.vue";
+
+const showComponent = ref(true);
+</script>
+
+<template>
+  <div>
+    <button @click="showComponent = !showComponent">
+      Toggle Child Component
+    </button>
+    <ChildComponent v-if="showComponent" />
+  </div>
+</template>
